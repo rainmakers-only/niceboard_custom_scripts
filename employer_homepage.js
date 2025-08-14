892f9c9b75adb76b3d32c8fdf2d7da0a02ac4d44
Vladimir Kozyrev <kozyrevvladimir@gmail.com>
2025-08-14 14:37:39 -0700

    - Execute

--- employer_homepage.js
+++ employer_homepage.js
@@ -1,11 +1,11 @@
-console.log("HELLO SCRIPTORO");
-
-window.onload = (event) => {
-  console.log("page is fully loaded");
-  console.log(document.getElementsByClassName("inner").item(2));
-  document
-    .getElementsByClassName("inner")
-    .item(2)
-    .innerHTML =
-    '<ol><li>One Community, Three Ways to Hire</li><li>One Community, Three Ways to Hire</li><li>One Community, Three Ways to Hire</li></ol>';
-};
+!function(){
+  window.onload = (event) => {
+    console.log("page is fully loaded");
+    console.log(document.getElementsByClassName("inner").item(2));
+    document
+      .getElementsByClassName("inner")
+      .item(2)
+      .innerHTML =
+      '<ol><li>One Community, Three Ways to Hire</li><li>One Community, Three Ways to Hire</li><li>One Community, Three Ways to Hire</li></ol>';
+  };
+}();
