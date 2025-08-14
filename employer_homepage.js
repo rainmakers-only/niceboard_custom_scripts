2dd776ec90cce42cfa3d1065469b87c1a98b2449
Vladimir Kozyrev <kozyrevvladimir@gmail.com>
2025-08-14 14:23:19 -0700

    - Initial commit

---
+++ employer_homepage.js
@@ -0,0 +1,4 @@
+console.log("page is fully loaded");
+console.log("HELLO SCRIPTORO");
+console.log(document.getElementsByClassName('inner').item(2));
+document.getElementsByClassName('inner').item(2).innerHTML = '<ol><li>One Community, Three Ways to Hire</li></ol>';
