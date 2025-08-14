a4258b4be122d1ee7d67c55d0b8b2779c7c639f3
Vladimir Kozyrev <kozyrevvladimir@gmail.com>
2025-08-14 14:25:40 -0700

    - Window.onload

--- employer_homepage.js
+++ employer_homepage.js
@@ -1,4 +1,10 @@
-console.log("page is fully loaded");
-console.log("HELLO SCRIPTORO");
-console.log(document.getElementsByClassName('inner').item(2));
-document.getElementsByClassName('inner').item(2).innerHTML = '<ol><li>One Community, Three Ways to Hire</li></ol>';
+window.onload = (event) => {
+  console.log("page is fully loaded");
+  console.log("HELLO SCRIPTORO");
+  console.log(document.getElementsByClassName("inner").item(2));
+  document
+    .getElementsByClassName("inner")
+    .item(2)
+    .innerHTML =
+    '<ol><li>One Community, Three Ways to Hire</li><li>One Community, Three Ways to Hire</li><li>One Community, Three Ways to Hire</li></ol>';
+};
